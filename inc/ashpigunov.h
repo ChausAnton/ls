1c411e5e71af9b2bb482fe09f3e0eda1ebfafd78
Anton Shpigunov <ashpigunov@c1r3p5.khpi.ucode-connect.study>
2020-12-21 14:43:21 +0200

    print ls -l

---
+++ inc/ashpigunov.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef HEADER_ls_ash
+#define HEADER_ls_ash
+
+#include "uls.h"
+
+void print_ls_l(struct t_ls_l **ls_l);
+
+void mx_print_time(char *t, struct stat *Stat);
+
+#endif
