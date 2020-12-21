fc2b74114514d2d10626b29f25444dff9592cad0
Anton Chaus <anchaus@c1r3p4.khpi.ucode-connect.study>
2020-12-21 19:36:23 +0200

    add work with many paths

--- src/main.c
+++ src/main.c
@@ -10,52 +10,60 @@ void clean_str_arr(char **arr) {
 
 int main(int argc, char *argv[]) {
 
-    char *path = NULL;
-    if(argc == 1){
-        path = mx_strdup("./");
-    } else if (argc == 2 && argv[1][0] == '-'){
-        path = mx_strdup("./");
-    } else if (argc == 2 && argv[1][0] != '-'){
-        path = mx_strdup(argv[1]);
-    } else if (argc == 3 && argv[1][0] != '-' && argv[2][0] == '-'){
-        path = mx_strdup(argv[1]);
-    } else if (argc >= 3 && argv[1][0] != '-' && argv[2][0] != '-' && argv[argc-1][0] != '-'){
-        printf("\n%d argc without flags\n\n", argc-1);
-        exit(0);
-    } else if (argc >= 4 && argv[1][0] != '-' && argv[2][0] != '-' && argv[argc-1][0] == '-'){
-        printf("\n%d argc with flags\n\n", argc-2);
-        exit(0);
+
+
+    char **path = (char **) malloc((argc + 1) * sizeof(char *));
+    for (int i = 0; i < argc + 1; i++){
+        path[i] = NULL;
     }
+
+    if(argc == 1) {
+        path[0] = mx_strdup("./");
+    }
+    if(argc == 2) {
+        if(mx_strcmp(argv[1], "-l") == 0) {
+            path[0] = mx_strdup("./");
+        }
+        else {
+            path[0] = mx_strdup(argv[1]);
+        }
+    }
+    if(argc > 2) {
+        int j = 0;
+        int i = 0;
+        if (mx_strcmp(argv[1], "-l") == 0) {
+            i = 2;
+        }
+        else{
+            i = 1;
+        }
+
+        for(; i < argc; i++) {
+            path[j] = mx_strdup(argv[i]);
+            j++;
+        }
         
-    char **arr = mx_ls(path);
+    }
 
-    
+    for(int g = 0; path[g] != NULL; g++) {
+        char **arr = mx_ls(path[g]);
 
-    if(mx_strcmp(argv[argc - 1], "-l") == 0) {
-        path = mx_strjoin(path, "/");
-        for(int i = 0; arr[i] != NULL; i++) {
-            arr[i] = mx_strjoin(path, arr[i]);
+        if(argc > 1 && mx_strcmp(argv[1], "-l") == 0) {
+            if(mx_strcmp(argv[1], "./") != 0)
+                path[g] = mx_strjoin(path[g], "/");
+            for(int i = 0; arr[i] != NULL; i++) {
+                arr[i] = mx_strjoin(path[g], arr[i]);
+            }
+            mx_ls_l(arr);
         }
-        mx_ls_l(arr);
-    }
-    else {
-    int size = 0;
-    for(int i = 0; arr[i] != NULL; i++) {
-        if(size < mx_strlen(arr[i])) {
-            size = mx_strlen(arr[i]);
+        else {
+            for(int i = 0; arr[i] != NULL; i++) {
+                mx_printstr(arr[i]);
+                mx_printstr("\n");
+            }
         }
+        clean_str_arr(arr);
     }
-
-    while (size % 4 != 0) {
-        size++;
-    }
-    size = size / 4;
-    
-    for(int i = 0; arr[i] != NULL; i++) {
-        mx_printstr(arr[i]);
-        mx_printstr("\n");
-    }
-    }
-    clean_str_arr(arr);
+    clean_str_arr(path);
 }
 
