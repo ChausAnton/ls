35a63341ff1817020065724b3d145b06612c7305
Anton Chaus <anchaus@c1r3p4.khpi.ucode-connect.study>
2020-12-21 19:52:01 +0200

    fix work with many dirs

--- src/main.c
+++ src/main.c
@@ -10,22 +10,20 @@ void clean_str_arr(char **arr) {
 
 int main(int argc, char *argv[]) {
 
-
-
-    char **path = (char **) malloc((argc + 1) * sizeof(char *));
+    char **paths = (char **) malloc((argc + 1) * sizeof(char *));
     for (int i = 0; i < argc + 1; i++){
-        path[i] = NULL;
+        paths[i] = NULL;
     }
 
     if(argc == 1) {
-        path[0] = mx_strdup("./");
+        paths[0] = mx_strdup("./");
     }
     if(argc == 2) {
         if(mx_strcmp(argv[1], "-l") == 0) {
-            path[0] = mx_strdup("./");
+            paths[0] = mx_strdup("./");
         }
         else {
-            path[0] = mx_strdup(argv[1]);
+            paths[0] = mx_strdup(argv[1]);
         }
     }
     if(argc > 2) {
@@ -39,20 +37,23 @@ int main(int argc, char *argv[]) {
         }
 
         for(; i < argc; i++) {
-            path[j] = mx_strdup(argv[i]);
+            paths[j] = mx_strdup(argv[i]);
             j++;
         }
-        
     }
 
-    for(int g = 0; path[g] != NULL; g++) {
-        char **arr = mx_ls(path[g]);
+    for(int g = 0; paths[g] != NULL; g++) {
+        char **arr = mx_ls(paths[g]);
+        
+        if(mx_strcmp(paths[g], "./") != 0 && (paths[g + 1] != NULL || g >= 1)) {
+            mx_printstr(paths[g]);
+            mx_printstr(":\n");
+        }
 
         if(argc > 1 && mx_strcmp(argv[1], "-l") == 0) {
-            if(mx_strcmp(argv[1], "./") != 0)
-                path[g] = mx_strjoin(path[g], "/");
+            paths[g] = mx_strjoin(paths[g], "/");
             for(int i = 0; arr[i] != NULL; i++) {
-                arr[i] = mx_strjoin(path[g], arr[i]);
+                arr[i] = mx_strjoin(paths[g], arr[i]);
             }
             mx_ls_l(arr);
         }
@@ -63,7 +64,9 @@ int main(int argc, char *argv[]) {
             }
         }
         clean_str_arr(arr);
+
+        if(paths[g + 1] != NULL) mx_printstr("\n");
     }
-    clean_str_arr(path);
+    clean_str_arr(paths);
 }
 
